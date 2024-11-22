38599d1742bd73afc16f5398c066d8cf032bff5a
blackbrokkoli <kastomat@gmail.com>
2024-11-22 14:00:24 +0100

    implement working quest prompt sound

--- src/components/LevelRenderer.vue
+++ src/components/LevelRenderer.vue
@@ -1,23 +1,25 @@
 <template>
     <div class="flex flex-col items-center ">
         <div class="">
-            <div v-if="currentQuest" class="flex flex-row gap-2 items-center card bg-base-200 shadow-xl p-2 my-2">
-                <button class="btn btn-circle btn-sm btn-secondary" @click="playQuestAudio">
-                    <svg v-if="!questSoundPlaying" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
-                    </svg>
+            <div class="min-h-14">
+                <div v-if="currentQuest" class="flex flex-row gap-2 items-center card bg-base-200 shadow-xl p-2 my-2">
+                    <button class="btn btn-circle btn-sm btn-secondary" @click="playQuestAudio">
+                        <svg v-if="!questSoundPlaying" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                        </svg>
 
-                    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                        stroke="currentColor" class="h-6 w-6 playing-speaker">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-                    </svg>
+                        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="h-6 w-6 playing-speaker">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+                        </svg>
 
-                </button>
-                <div class="card-title">
-                    {{ questPrompt }}
+                    </button>
+                    <div class="card-title">
+                        {{ questPrompt }}
+                    </div>
                 </div>
 
             </div>
@@ -39,7 +41,6 @@
     </div>
 
     <SoundEffectPlayer ref="soundEffectPlayer"></SoundEffectPlayer>
-    <button class="btn" @click="handleSuccess">play success</button>
 
 </template>
 
@@ -137,23 +138,30 @@ function startRandomQuestFromList() {
     if (questsWithoutLast.length > 0) {
         currentQuest.value = questsWithoutLast[Math.floor((Math.random() * questsWithoutLast.length))]
         lastQuest.value = currentQuest.value
-        playQuestAudio()
     }
 }
 
 function endCurrentQuest(questWasSuccessful: boolean) {
+    if (questWasSuccessful) {
+        handleSuccess()
+    }
     currentQuest.value = undefined
-    startRandomQuestFromList()
+    setTimeout(startRandomQuestFromList, 1000);
 }
 
 function playQuestAudio() {
     const audioPlayer = document.getElementById('prompt-audio')
     if (audioPlayer !== null) {
-        // @ts-ignore: let's trust that this is in fact an audio player, yes
+        // @ts-ignore: let's trust that this is in fact an audio player
         audioPlayer.play();
         questSoundPlaying.value = true;
     }
 }
+
+watch(currentQuest, () => {
+    setTimeout(playQuestAudio, 500)
+    playQuestAudio()
+}, { immediate: true })
 
 function handleSuccess() {
     if (soundEffectPlayer.value !== undefined) {
